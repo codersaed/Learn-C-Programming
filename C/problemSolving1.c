189b8f28701cbb406c282668b4666446b56e948c
codersaed <codersaed@gmail.com>
2022-07-01 21:24:22 +0600

    ps1

---
+++ C/problemSolving1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main() {
+    int number;
+    scanf("%d", &number);
+
+    if(number %2 == 0){
+        printf("Number is even");
+    }
+    else{
+        printf("Number is odd");
+    }
+}
+
+
+int main(){
+  int number1;
+  scanf("%d", &number1);
+
+  if(number1 < 0){
+    printf("-1");
+  }
+  else if(number1 > 0){
+    printf("1");
+  }
+  else{
+    printf("0");
+  }
+}
+
+
+
+
+
+
